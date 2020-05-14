2abf293f7bc66bf7ccf35c5cd860118d19d5fea1
Marcos Diógenes da Silva Junior <mjunior@leega.local>
2020-05-14 02:09:13 -0300

    x

--- angular-bootstrap-example2/src/app/cursos/cursos.service.ts
+++ angular-bootstrap-example2/src/app/cursos/cursos.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 
-@Injectable({
+@Injectable(/*{
   providedIn: 'root'
-})
+}*/)
 export class CursosService {
 
   cursos: string[] = ['java','kotlin'];
